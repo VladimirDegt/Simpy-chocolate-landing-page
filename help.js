a51043be8b904d41415719d9a36727cc39ec29b6
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-03-16 21:43:20 +0200

    добавил icons

---
+++ help.js
@@ -0,0 +1,12 @@
+header, hero - Oleg
+
+Benefits - Володимир
+
+section_1 з 4 картками - Adelina
+
+section_2 з видео - Yanislav Kutsenko
+
+section_4 зі слайдером - Мельник Олексій
+
+Top Sellers - Danyil
+ 
